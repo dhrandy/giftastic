e9d2d11273d17e3c879014ee6585feffa0f99b3d
dhrandy <dhrandy@gmail.com>
2018-04-18 20:30:25 -0400

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,7 @@ $( document ).ready(function() {
 
 // Initial array of animals
 var animals = ["skunk", "squirl"];
+console.log("animalInitialArray = ",animals)
 
 function displayAnimalInfo () {
 
@@ -26,25 +27,50 @@ $.ajax({
   for (i = 0; i < response.data.length; i ++){
 
   // Storing the rating data
+  
   var ratings = response.data[i].rating;
 
   // Creating an element to have the rating displayed
+  if (response.data[i].rating !== "g" || "pg" || "pg-13" || "y"){
   var ratingElement = $("<p>").text("Rating: " + ratings);
-
-  // Retrieving the URL for the image
-  var imgURL = response.data[i].images.fixed_height.url;    
+  }
+  // Retrieving the URL for the still image
+  var stillImgURL = response.data[i].images.fixed_height_still.url;
+  // Retrieving the URL for the giphy 
+  var giphImgUrl = response.data[i].images.fixed_height.url;
+  // Retrieving the URL for the class
+  var giphClass = response.data[i].type;
 
   // Creating an element to hold the image
-  var image = $("<img>").attr("src", imgURL);
+  var image = $("<img>").attr("src", stillImgURL)
+  .attr("data-still", stillImgURL)
+  .attr("data-animate", giphImgUrl)
+  .attr("data-state", "still")
+  .attr("class", giphClass);
 
   // Appending the image
   animalDiv.append(ratingElement);
   animalDiv.append(image);
-
+  
   $("#gifs-appear-here").prepend(animalDiv);
 
+  $(".gif").on("click", function() {
+    // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+    var state = $(this).attr("data-state");
+    // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+    // Then, set the image's data-state to animate
+    // Else set src to the data-still value
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+      console.log("animate");
+    } else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+      console.log("still");
+    }
+  });
   }
-
 });
 
 }
@@ -69,6 +95,7 @@ function renderButtons() {
     a.text(animals[i]);
     // Adding the button to the buttons-view div
     $("#animals-appear-here").append(a);
+    console.log("animalArray = ",animals)
   }
 }
 
@@ -82,6 +109,7 @@ $("#addAnimal").on("click", function(event) {
 
   // Adding animal from the textbox to our array
   animals.push(animalFromInput);
+  
 
   // Calling renderButtons which handles the processing of our animal array
   renderButtons();
@@ -93,5 +121,6 @@ $("#addAnimal").on("click", function(event) {
 
 renderButtons();
 });
+
 
 
